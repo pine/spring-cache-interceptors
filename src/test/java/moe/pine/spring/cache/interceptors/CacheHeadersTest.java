cdda6e04ad7fa7ae8b2e91e6919ba4ed17c288d1
Pine Mizune <pinemz@gmail.com>
2019-06-12 14:13:38 +0900

    test: add test [WIP]

--- src/test/java/moe/pine/spring/cache/interceptors/CacheHeadersTest.java
+++ src/test/java/moe/pine/spring/cache/interceptors/CacheHeadersTest.java
@@ -2,11 +2,27 @@ package moe.pine.spring.cache.interceptors;
 
 import org.junit.Test;
 
+import java.time.Clock;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
 import static org.junit.Assert.assertEquals;
 
 public class CacheHeadersTest {
+    private static final ZoneOffset ZONE_OFFSET = ZoneOffset.of("Asia/Tokyo");
+    private static final Clock CLOCK =
+            Clock.fixed(
+                    ZonedDateTime
+                            .of(2019, 6, 12, 12, 0, 0, 0, ZONE_OFFSET)
+                            .toInstant(),
+                    ZONE_OFFSET);
+
     @Test
     public void cacheControlTest() {
         assertEquals("Cache-Control", CacheHeaders.CACHE_CONTROL.getName());
+
+        final CachePolicy cachePolicy = new CachePolicyBuilder()
+                .build();
+        CacheHeaders.CACHE_CONTROL.buildValue(cachePolicy, CLOCK);
     }
 }
