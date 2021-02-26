3d966ca51c48e9f054a4063f21dcb23ba0918821
Pine Mizune <pinemz@gmail.com>
2021-02-26 18:51:03 +0900

    chore: cosmetic change

--- src/main/java/moe/pine/spring/cache/interceptors/package-info.java
+++ src/main/java/moe/pine/spring/cache/interceptors/package-info.java
@@ -1,3 +1,6 @@
-@org.springframework.lang.NonNullApi
-@org.springframework.lang.NonNullFields
+@NonNullApi
+@NonNullFields
 package moe.pine.spring.cache.interceptors;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
